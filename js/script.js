7e344ff25a4572a072e0fe1e1d9a129fecb1b867
Helen Virtanen <helenvirtanen28@gmail.com>
2025-03-16 15:13:08 +0300

    Added delegation to slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* Gallery slider */
 
+/* Var 1 - with loop 
+
 let photos = document.querySelectorAll('.gallery__photo');
 let photoActive = document.querySelector('.gallery__photo_active');
 
@@ -8,3 +10,22 @@ for (let i = 0; i < photos.length; i++) {
         evt.preventDefault();
         photoActive.src = photos[i].src;});
 };
+
+*/
+
+/* Var 2 - with delegation */
+
+let photos = document.getElementById('photos');
+
+photos.onclick = function(event) {
+    let photo = event.target.closest('img');
+
+    if (!photo) return;
+    showPhoto(photo.src, photo.alt);
+    event.preventDefault();
+}
+
+function showPhoto(src, alt) {
+    largePhoto.src = src;
+        largePhoto.alt = alt;
+}
